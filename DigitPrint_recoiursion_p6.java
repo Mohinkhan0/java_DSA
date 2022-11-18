db9fcfec21a2f3fd4a003b4070343ec4ab9134e4
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2022-11-18 09:27:52 -0800

    Add files via upload

--- DigitPrint_recoiursion_p6.java
+++ DigitPrint_recoiursion_p6.java
@@ -3,15 +3,25 @@ public class DigitPrint_recoiursion_p6
     private static int rem;
     static int digit(int num)
     {
-        if(num==0)
+              if(num!=0)
         {
-           int  rem=num%10;
-           digit(num/10);
+          rem=num%10;
+          System.out.println(rem);
+          digit(num/10);
         }
         return rem;
+
     }
     public static void main(String[] args) 
     {
-   int  rem=digit(12345);
+        int r=digit(12345);
     } 
 }
+ /*
+output
+5
+4
+3
+2
+1
+  */
