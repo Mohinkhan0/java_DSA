2e1b43afd98004793e7a7fd7716507c514a52dc4
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2023-03-05 23:53:12 +0530

    Add files via upload

---
+++ Patterns/ReverseTringle.java
@@ -0,0 +1,15 @@
+public class ReverseTringle 
+{
+     public static void main(String[] args) 
+     {
+         int i,j,row=5;
+         for(i=5 ; i>0 ; i--)
+         {
+          for(j=0 ; j<i ;j++)
+          {
+            System.out.print("*");
+          }
+          System.out.println();
+         }
+     }
+}
