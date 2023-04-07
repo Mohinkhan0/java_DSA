5661b3c14f856f32ea881f725632470c8fbb0253
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2023-04-07 05:55:31 +0530

    Add files via upload

--- Array/2D Array/Snackpattern.java
+++ Array/2D Array/Snackpattern.java
@@ -1,1 +1,33 @@
+public class Snackpattern 
+{
+    public static void main(String[] args) 
+    {
+        int array[][]={{1,2,5,6},
+                       {4,7,3,2},
+                      {12,15,10}};
+                      snack(array);
+    }
 
+    private static void snack(int[][] array) 
+    {
+        for(int i=0; i<array.length; i++)
+        {
+             if(i%2==0)
+             {
+                for(int j=0; j<array[i].length; j++)
+                {
+                 System.out.print(array[i][j] + " ");
+                }
+             }
+             else
+             {
+                for(int j=array[i].length-1; j>=0 ;j--)
+                {
+                 System.out.print(array[i][j] + " ");
+                }
+             }
+             System.out.println();
+        }
+    }
+
+}
